adea94efc51d8626c5ec59eb923700aff365fec0
nimeshpatel16 <nimesh.patel16@hotmail.com>
2019-07-15 08:39:50 +0530

    Changes

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-
+//Comments
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
